b7da51d1e98c03ab525c8420831a8ec0194ff313
shuuraku <oillusory@gmail.com>
2020-07-17 23:09:59 +0800

    Java and kotlin var,data type

--- src/main/java/com/learn/hello.java
+++ src/main/java/com/learn/hello.java
@@ -4,5 +4,21 @@ public class hello {
     public static void main(String[] args) {
         Person p = new Person();
         p.hello();
+        int i =32; //位元
+
+        Integer age2 = 19;
+        age2.byteValue();
+
+        short s = 16; //位元
+        long l = 64; //位元
+        char c = 'A'; //單字元單引號
+        Character c2 ='A';
+
+        byte b = 8;//位元
+        double d = 6.6;
+        float f = 1.1f;
+        boolean tf = true;
+        String xd = "AAA"; //多字元雙引號
+
     }
 }
--- src/main/kotlin/com/learn/hello.kt
+++ src/main/kotlin/com/learn/hello.kt
@@ -4,6 +4,14 @@ fun main() {
 //    println("hello")
     val h = Human()
     h.hello()
+    val age : Int = 19
+    var age2 : Int = 18
+    age2 = 19
+    var f = 1.1f
+    var f2 : Float = 1.1f
+    val name = "AA"
+    var name2 : String
+
 }
 class Human {
     fun hello(){

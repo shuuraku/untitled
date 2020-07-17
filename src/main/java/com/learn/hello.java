dff22c5f8c172aa9f8edc5d53f10bd3f4e71bd28
shuuraku <oillusory@gmail.com>
2020-07-17 19:44:19 +0800

    1

---
+++ src/main/java/com/learn/hello.java
@@ -0,0 +1,8 @@
+package com.learn;
+
+public class hello {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.hello();
+    }
+}
---
+++ src/main/kotlin/com/learn/hello.kt
@@ -0,0 +1,12 @@
+package com.learn
+
+fun main() {
+//    println("hello")
+    val h = Human()
+    h.hello()
+}
+class Human {
+    fun hello(){
+        println("hello kotline")
+    }
+}

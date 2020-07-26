7b4240a6a6153e2480e1ca4e23c54c233c116a7d
shuuraku <oillusory@gmail.com>
2020-07-26 16:31:23 +0800

    Unit13 屬性、建構子與方法的類別，整合練習

---
+++ src/main/java/com/learn/student/SchoolRunner.java
@@ -0,0 +1,8 @@
+package com.learn.student;
+
+public class SchoolRunner {
+    public static void main(String[] args) {
+        Student stu = new Student("S",66,96);
+        stu.print();
+    }
+}

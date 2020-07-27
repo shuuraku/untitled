f6a338f2df6fc54ced0829808d8afe5a34c30301
shuuraku <oillusory@gmail.com>
2020-07-27 14:26:21 +0800

    Unit14 import類別(使用者輸入)

--- src/main/java/com/learn/student/SchoolRunner.java
+++ src/main/java/com/learn/student/SchoolRunner.java
@@ -1,8 +1,17 @@
 package com.learn.student;
 
+import java.util.Scanner;
+
 public class SchoolRunner {
     public static void main(String[] args) {
-        Student stu = new Student("S",66,96);
+        System.out.println("請輸入名字：");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        System.out.print("請輸入英文成績：");
+        int english = scanner.nextInt();
+        System.out.print("請輸入數學成績");
+        int math = scanner.nextInt();
+        Student stu = new Student(name,english,math);
         stu.print();
     }
 }
--- src/main/kotlin/com/learn/student/Student.kt
+++ src/main/kotlin/com/learn/student/Student.kt
@@ -1,7 +1,16 @@
 package com.learn.student
 
+import java.util.*
+
 fun main() {
-    val stu = Student("SS",66,88)
+    val scanner = Scanner(System.`in`)
+    print("請輸入名字：")
+    var name = scanner.next()
+    print("請輸入英文成績：")
+    var english = scanner.nextInt()
+    print("請輸入數學成績：")
+    var math = scanner.nextInt()
+    val stu = Student(name, english, math)
     stu.print()
 }
 

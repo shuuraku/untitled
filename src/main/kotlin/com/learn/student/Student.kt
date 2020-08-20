3d5a9c899d25e69ca8298fd9157e043bd78d56aa
shuuraku <oillusory@gmail.com>
2020-08-20 21:03:21 +0800

    Unit19 Kotlin的判斷式when、更實用的寫法

--- src/main/kotlin/com/learn/student/Student.kt
+++ src/main/kotlin/com/learn/student/Student.kt
@@ -10,27 +10,51 @@ fun main() {
 }
 
 class Student(var name: String,var english:Int,var math:Int){
-    fun highest():Int{
-        var max = if (math>english) math else english
-       /* var max = if (math>english) {
+//----------------------
+    fun highest() = if (math>english) math else english
+       /* fun highest():Int {
+       var max = if (math>english) math else english
+        var max = if (math>english) {
             println("math　")
             math
         } else {
             println("english　")
             english
-        }*/
+        }
         return max
-    }
-
+    }*/
+//----------------------
     fun print(){
-        println(name+"\t"+english+"\t"+math+"\t"+ getAverage() + if (getAverage()>=60) "\tPASS" else "\tFAILED")
+        println("$name\t$english\t$math\t${getAverage()}\t${if (getAverage()>=60) "PASS" else "FAILED"}\t${grading()}")
+        /*print(name+"\t"+english+"\t"+math+"\t"+ getAverage() + if (getAverage()>=60) "\tPASS" else "\tFAILED")
+        println("\t"+ grading())*/
     }
+//-----------------------
+    fun grading() = when(getAverage()){
+        in 90..100 -> 'A'
+        in 80..89 -> 'B'
+        in 70..79 -> 'C'
+        in 60..69 -> 'D'
+        else -> 'F'
+    }
+    /*fun grading():Char{
+        var grading:Char = when(getAverage()){
+            in 90..100 -> 'A'
+            in 80..89 -> 'B'
+            in 70..79 -> 'C'
+            in 60..69 -> 'D'
+            else -> 'F'
+        }
+        return grading
+    }*/
+//------------------------
 
-    fun getAverage():Int{
+    /*fun getAverage():Int{
         return (english+math)/2
-    }
+    }*/
+    fun getAverage() = (english+math)/2
 }
-
+//------------------------
 private fun userinput() {
     val scanner = Scanner(System.`in`)
     print("請輸入名字：")

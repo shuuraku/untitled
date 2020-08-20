78b7b9203dd469b82b8c84325c68f03d7161cc06
shuuraku <oillusory@gmail.com>
2020-08-20 20:27:32 +0800

    Unit18 Java的switch...case

--- src/main/java/com/learn/student/SchoolRunner.java
+++ src/main/java/com/learn/student/SchoolRunner.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SchoolRunner {
     public static void main(String[] args) {
 //        userinput();
-        Studentj stu = new Studentj("raku",60,90);
+        Studentj stu = new Studentj("raku",60,100);
         stu.print();
         System.out.println(stu.highest());
     }
--- src/main/java/com/learn/student/Studentj.java
+++ src/main/java/com/learn/student/Studentj.java
@@ -24,12 +24,42 @@ public class Studentj {
 
     public void print(){
         System.out.print(name + "\t" + english + "\t" + math
-                + "\t" + getAverage());
-        if (getAverage()>=60){
+                + "\t" + getAverage() + ((getAverage()>=60)? "\tPASS":"\tFAILED"));
+        char grading ;
+        switch (getAverage()/10){
+            case 10:
+            case 9:
+                grading = 'A';
+                break;
+            case 8:
+                grading ='B';
+                break;
+            case 7:
+                grading = 'C';
+                break;
+            case 6:
+                grading = 'D';
+                break;
+            default:
+                grading = 'F';
+        }
+        System.out.println("\t"+grading);
+        /*if(getAverage()>=90 && getAverage() <=100){
+            grading = 'A';
+        }else if (getAverage()>=80 && getAverage() <90){
+            grading = 'B';
+        }else if (getAverage()>=70 && getAverage() <80){
+            grading ='C';
+        }else if (getAverage()>=60 && getAverage()<70){
+            grading ='D';
+        }else {
+            grading ='F'
+        }*/
+      /*  if (getAverage()>=60){
             System.out.println("\tPASS");
         }else {
             System.out.println("\tFAILED");
-        }
+        }*/
     }
     public int getAverage(){
         return (english+math)/2;

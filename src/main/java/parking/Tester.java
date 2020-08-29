a3848b636e612fc778b4a3eeb790b4c067dffa41
shuuraku <oillusory@gmail.com>
2020-08-29 20:13:25 +0800

    Unit24 Java8 新的日期時間處理。

--- src/main/java/parking/Tester.java
+++ src/main/java/parking/Tester.java
@@ -2,11 +2,33 @@ package parking;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tester {
     public static void main(String[] args) throws ParseException {
+        //java8
+        Instant instant = Instant.now();
+        System.out.println(instant+"\n");
+        //Local
+        LocalDateTime local = LocalDateTime.now();
+        System.out.println(local);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println(formatter.format(local)+"\n");
+        System.out.println(local.plus(Duration.ofHours(3)));
+        System.out.println(formatter.format(local.plus(Duration.ofHours(3)))+"\n");
+        LocalDateTime other
+                = LocalDateTime.of(2020,12,19,12,19);
+        System.out.println(other);
+        System.out.println(formatter.format(other));
+//        java();
+    }
+
+    private static void java() throws ParseException {
         Date date = new Date();
         System.out.println(date);
         System.out.println(date.getTime());

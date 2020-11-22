40370500c97ab0f19ec919186a72fe3aac62806c
shuuraku <oillusory@gmail.com>
2020-11-22 14:49:58 +0800

    Unit27 Java Set與Map集合、Kotlin計算停車場費用

--- src/main/java/com/learn/many/Tester.java
+++ src/main/java/com/learn/many/Tester.java
@@ -1,11 +1,38 @@
 package com.learn.many;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
+        HashMap<String,String> stocks = new HashMap<>();
+        stocks.put("2330","台積電");
+        stocks.put("2317","鴻海");
+        stocks.put("2330","TSMC");
+        System.out.println(stocks.get("2330"));
+        System.out.println(stocks);
+        for (String key : stocks.keySet()){
+            System.out.println(stocks.get(key));
+        }
+//        set();
+//        list();
+//        arrayTest();
+    }
+
+    private static void set() {
+        //set
+        HashSet<Integer> set = new HashSet<>();
+        set.add(6);
+        set.add(7);
+        set.add(1);
+        set.add(8);
+        set.add(7);
+        System.out.println(set);
+        for (int n : set){
+            System.out.println(n);
+        }
+    }
+
+    private static void list() {
         ArrayList<Integer> list = new ArrayList();
         list.add(5);
         list.add(3);
@@ -19,8 +46,6 @@ public class Tester {
         for (int score:scores){
             System.out.println(score);
         }
-
-//        arrayTest();
     }
 
     private static void arrayTest() {
--- src/main/kotlin/com/learn/many.kt
+++ src/main/kotlin/com/learn/many.kt
@@ -1,7 +1,29 @@
 package com.learn
 
+import java.time.LocalDateTime
+
 fun main() {
-    val list = listOf(5,1,2,7)
+    val parkingLot = HashMap<String,Car?>()
+    var enter = LocalDateTime.of(2018,12,25,8,0,0)
+    var car:Car? = Car("AAA-0001",enter)
+    parkingLot.put(car!!.id,car)
+    car = Car("BBB-0002",enter.plusMinutes(15))
+    parkingLot.put(car.id,car)
+    // Car 1 leaving
+    var leave = LocalDateTime.of(2018,12,25,9,0,0)
+    car = parkingLot.get("AAA-0001")
+    car?.leave = leave
+    println("${car?.id} duration: ${car?.duration()}")
+    parkingLot.remove(car?.id)
+    println(parkingLot.size)
+    // Car 2 leaving
+    car = parkingLot.get("BBB-0002")
+    car?.leave = leave.plusHours(2)
+    println("${car?.id} duration: ${car?.duration()}")
+    parkingLot.remove(car?.id)
+    println(parkingLot.size)
+
+/*    val list = listOf(5,1,2,7)
     println(list)
     val scores = listOf(68,88,77,99,50)
     for (score in scores){
@@ -10,5 +32,5 @@ fun main() {
     println(list.get(3))
     var mutableList = mutableListOf(5,1,2,7)
     mutableList.add(6)
-    println(mutableList)
+    println(mutableList)*/
 }

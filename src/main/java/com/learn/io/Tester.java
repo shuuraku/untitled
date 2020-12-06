d394e4c01fdc3613171f2a48712c48070c4af25e
raku <oillusory@gmail.com>
2020-12-06 20:34:57 +0800

    Unit34 Java輸出入相關類別與緩衝區讀取。

--- src/main/java/com/learn/io/Tester.java
+++ src/main/java/com/learn/io/Tester.java
@@ -1,20 +1,58 @@
 package com.learn.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            String line = br.readLine();
+            while ( line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        filereader();
+//        inputstream();
+    }
+
+    private static void filereader() {
+        try {
+            FileReader fr = new FileReader("data.txt");
+            int n = fr.read();
+            while (n != -1){
+                System.out.print((char)n);
+                n = fr.read();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void inputstream() {
         File file = new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
         try{
             InputStream is = new FileInputStream("data.txt");
+            int n = is.read();
+            while (n != -1){
+                System.out.print((char)n);
+                n = is.read();
+            }
+            System.out.println();
         }catch (FileNotFoundException e){
             e.printStackTrace();
             System.out.println("message");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("File open success.");
     }
